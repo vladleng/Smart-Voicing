fa5dff46f1c1bc6b5e5512de3f223c2369866bfb
Vladislav <vladislavpaderin@gmail.com>
2026-09-19 22:18:15 +0700

    Сделать Smart Voicing ARA чистым Event FX reader

--- src/PluginProcessor.h
+++ src/PluginProcessor.h
@@ -23,8 +23,8 @@ public:
 
     const juce::String getName() const override { return JucePlugin_Name; }
 
-    bool acceptsMidi() const override { return true; }
-    bool producesMidi() const override { return true; }
+    bool acceptsMidi() const override { return false; }
+    bool producesMidi() const override { return false; }
     bool isMidiEffect() const override { return false; }
     double getTailLengthSeconds() const override { return 0.0; }
 
